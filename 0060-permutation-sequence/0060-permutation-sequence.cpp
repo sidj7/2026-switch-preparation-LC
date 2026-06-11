69ba31b71348f0e61fbd2ec459f012b0c7b5f4aa
Siddharth Jain <sdzen.sid@gmail.com>
2026-06-11 23:19:49 +0530

    Time: 0 ms (100%), Space: 7.9 MB (94.9%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
+        //vector<int> arr(n); iota(arr.begin(), arr.end(), 1);  populate arr with 1 to N;
+
         vector<int>nums;
+        k--;
         int fact=1;
-        string ans="";
-        k=k-1;
         for(int i=1;i<n;i++){
             nums.push_back(i);
-            fact=fact*i;
+            fact*=i;
         }
         nums.push_back(n);
-        while(1){
-            ans+=to_string(nums[k/fact]);
+        string ans="";
+        while(true){
+            ans+=('0'+nums[k/fact]);
             nums.erase(nums.begin()+k/fact);
             if(nums.size()==0) break;
 
-            k%=fact;
-            fact/=nums.size();
+            k=k%fact;
+            fact=fact/nums.size();
         }
         return ans;
+
     }
 };
