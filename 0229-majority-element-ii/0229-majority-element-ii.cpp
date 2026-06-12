e46ea9d8a4dc19b55b794aa4c672a07909cc9223
Siddharth Jain <sdzen.sid@gmail.com>
2026-06-12 23:39:15 +0530

    Time: 0 ms (100%), Space: 23.8 MB (43.69%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int el1,el2;
-        int cnt1=0,cnt2=0;
-        int k=(nums.size())/3;
-        vector<int>ans;
+        int el1,el2,cnt1=0,cnt2=0;
+        int n=nums.size();
         for(int i:nums){
             if(cnt1==0 && i!=el2){
+                cnt1=1;
                 el1=i;
-                cnt1++;
             }
             else if(cnt2==0 && i!=el1){
+                cnt2=1;
                 el2=i;
-                cnt2++;
             }
             else if(i==el1) cnt1++;
             else if(i==el2) cnt2++;
 
-            else {
-                cnt1--;
-                cnt2--;
+            else{
+                cnt1--;cnt2--;
             }
-       }
-       cnt1=0,cnt2=0;
-       for(int i:nums){
-        if(i==el1) cnt1++;
-        if(i==el2) cnt2++;
-       }
-       if(cnt1>k && cnt2>k){
-        return {el1,el2};
-       }
-       else if(cnt1>k){
-        return {el1};
-       }
-       else if(cnt2>k){
-        return {el2};
-       }
-       return{};
+        }
+        cnt1=0;
+        cnt2=0;
+        for(int i:nums){
+            if(i==el1) cnt1++;
+            else if(i==el2) cnt2++;
+        }
+        if(cnt1>n/3 && cnt2>n/3) return {el1,el2};
+        else if(cnt1>n/3) return {el1};
+        else if(cnt2>n/3) return {el2};
+        return {};
     }
 };
