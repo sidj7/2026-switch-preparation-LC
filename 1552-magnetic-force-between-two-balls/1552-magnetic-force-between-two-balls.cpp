ad5af036af00f4995646677f33004e2ca1929d25
Siddharth Jain <sdzen.sid@gmail.com>
2026-06-29 20:29:09 +0530

    Time: 54 ms (28.89%), Space: 61.8 MB (37.97%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
 
-    bool isval(vector<int>&pos,int m,int f){
+    bool isval(vector<int>&pos,int b,int mf){
         int cnt=1,last=pos[0];
-
         for(int i=1;i<pos.size();i++){
-            if(pos[i]-last>=f){
+            if(pos[i]-last>=mf){
                 cnt++;
                 last=pos[i];
+                if(cnt>=b) return true;
             }
         }
-        return cnt>=m;
+        return false;
     }
     int maxDistance(vector<int>& pos, int m) {
         sort(pos.begin(),pos.end());
         int h=pos.back()-pos[0];
         int l=1;
+
         while(l<=h){
+            int md=l+(h-l)/2;
 
-            int mid=l+(h-l)/2;
-
-            if(isval(pos,m,mid)){
-                l=mid+1;
+            if(isval(pos,m,md)){
+                l=md+1;
             }
-            else h=mid-1;
+            else h=md-1;
         }
         return h;
     }
