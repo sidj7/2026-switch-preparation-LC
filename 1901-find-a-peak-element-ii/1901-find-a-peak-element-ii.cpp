4fb670d36ae7755a7bb3120eca1d1cb11abf8012
Siddharth Jain <sdzen.sid@gmail.com>
2026-07-01 02:02:15 +0530

    Time: 0 ms (100%), Space: 50.2 MB (71.61%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-
-    int func(vector<vector<int>>& mat,int c,int r){
-        int el=INT_MIN;
-        int ans=-1;
-        for(int i=0;i<r;i++){
-            if(mat[i][c]>el){
-                el=mat[i][c];
-                ans=i;
+    int find(vector<vector<int>>& arr,int c){
+        int ans=0,idx=-1;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i][c]>ans){
+                ans=arr[i][c];
+                idx=i;
             }
         }
-        return ans;
+        return idx;
     }
+
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
 
         int l=0,h=m-1;
-
+        pair<int,int>ans;
         while(l<=h){
+            int c=l+(h-l)/2;
 
-            int md=(l+h)/2;
+            int r=find(mat,c);
 
-            int r=func(mat,md,n);
+            int left=c>0?mat[r][c-1]:-1;
+            int right = c<m-1?mat[r][c+1]:-1;
 
-            int left=md-1>=0?mat[r][md-1]:-1;
-            int right=md+1<m?mat[r][md+1]:-1;
-
-            if(mat[r][md]>left && mat[r][md]>right) return {r,md};
-
-            else if(mat[r][md]<right)l=md+1;
-            else h=md-1; 
-
+            if(mat[r][c]>left && mat[r][c]>right){
+                return {r,c};
+            }
+            else if(mat[r][c]<left){
+                h=c-1;
+            }
+            else l=c+1;
         }
-
         return {-1,-1};
     }
 };
