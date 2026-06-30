ac8472b1b7dfc17c34d3fe8a33b212e4b2fb5f21
Siddharth Jain <sdzen.sid@gmail.com>
2026-06-30 16:18:03 +0530

    Time: 3 ms (7.68%), Space: 10.4 MB (72.62%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
-    bool isval(vector<int>& nums, int k,int m){
-        int arrcnt=1,sum=0;
-        for(int i:nums){
-            if(sum+i<=m) sum+=i;
 
+    bool isval(vector<int>&nums,int k,int m){
+        int sum=0;
+        int cnt=1;
+        for(int i:nums){
+            if(i>m) return false;
+
+            if(sum+i<=m){
+                sum+=i;
+            }
             else{
                 sum=i;
-                arrcnt++;
+                cnt++;
             }
-
         }
-        return arrcnt<=k;
+        return cnt<=k;
     }
-
     int splitArray(vector<int>& nums, int k) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        if(k==1) return accumulate(nums.begin(),nums.end(),0);
-        if(k==n) return *max_element(nums.begin(),nums.end());
-
         int l=*max_element(nums.begin(),nums.end());
         int h=accumulate(nums.begin(),nums.end(),0);
 
+        int n=nums.size();
+        if(n==1) return nums[0];
+        if(k==1) return h;
+        if(n==k) return l;
+
         while(l<=h){
+
             int m=l+(h-l)/2;
-            if(isval(nums,k,m)) h=m-1;
+
+            if(isval(nums,k,m)){
+                h=m-1;
+            }
             else l=m+1;
+
         }
         return l;
     }
